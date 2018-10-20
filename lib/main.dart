3e1fa4b0a0f03882b116291c28b6f2935bebbd51
adrfeudj <adrfeudj@cisco.com>
2018-10-20 01:13:46 -0400

    Add chat screen class

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,30 @@
-// Create an infinite scrolling lazily loaded list
-
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new FriendlychatApp());
+}
 
-class MyApp extends StatelessWidget {
+class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Startup Name Generator',
-      //theme: new ThemeData.dark(),
-      home: new RandomWords(),
+      title: "Friendlychat",
+      theme: new ThemeData.dark(),
+      home: new ChatScreen(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class ChatScreen extends StatefulWidget {
   @override
-  RandomWordsState createState() => new RandomWordsState();
+  State createState() => new ChatScreenState();
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = new Set<WordPair>();
-  final TextStyle _biggerFont = const TextStyle(fontSize: 20.0);
-  void _pushSaved() {
-    Navigator.of(context).push(
-
-        new MaterialPageRoute<void>(   // Add 20 lines from here...
-          builder: (BuildContext context) {
-            final Iterable<ListTile> tiles = _saved.map(
-                  (WordPair pair) {
-                return new ListTile(
-                  title: new Text(
-                    pair.asPascalCase,
-                    style: _biggerFont,
-                  ),
-                );
-              },
-            );
-            final List<Widget> divided = ListTile
-                .divideTiles(
-              context: context,
-              tiles: tiles,
-            )
-                .toList();
-
-            return new Scaffold(
-              appBar: new AppBar(
-                title: const Text('Saved Suggestions'),
-              ),
-              body: new ListView(children: divided),
-            );
-          },
-        ),
-    );// ... to here.
-  }
+class ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: const Text('Random Name Generator'),
-        actions: <Widget>[
-          new IconButton(icon: const Icon(Icons.playlist_play ), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) {
-            return const Divider();
-          }
-          final int index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.check_circle : Icons.check_circle_outline,
-        color: alreadySaved ? Colors.blue : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
+      appBar: new AppBar(title: new Text("Friendlychat")),
     );
   }
 }
